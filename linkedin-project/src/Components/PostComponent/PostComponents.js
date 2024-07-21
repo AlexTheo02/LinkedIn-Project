d5e3249d0e580236445755dd86800ce1d3b69595
Alexandros Theofylaktou <sdi2100220@di.uoa.gr>
2024-07-21 17:54:20 +0300

    home page branch

--- linkedin-project/src/Components/PostComponent/PostComponents.js
+++ linkedin-project/src/Components/PostComponent/PostComponents.js
@@ -5,20 +5,39 @@ import {useState} from "react"
 import likedButtonImg from "../../Images/LikedButton.png"
 import notLikedButtonImg from "../../Images/LikeButton.png"
 import { useNavigate } from "react-router-dom";
+import { HorizontalSeparator, VerticalSeparator } from "../Separators/Separators";
+
+import tsipras from "../../Images/tsipras.jpg"
+import mitsotakis from "../../Images/mitsotakis.jpg"
 
 
-function InteractiveProfile({profilePicture, userName}){
-    
+
+function InteractiveProfile({user_id, altern}){
+    // Replace with database access
+    var profilePicture,userName
+    if (user_id === 3){
+        profilePicture = tsipras;
+        userName = "Alexis Tsipras";
+    }
+    if (user_id === 2){
+        profilePicture = mitsotakis;
+        userName = "Kyriakos Mitsotakis"
+    }
+
+
+    // Change arguments to user_id, then get pfp and username from database
     const navigate = useNavigate();
 
     const handleProfileClick = () => {
         navigate("/");
     }
 
+    const style = altern ? s.alt_interactive_profile : s.interactive_profile
+
     return(
         // Should be clickable and redirect you to the user's profile
-        <div onClick={handleProfileClick} className={s.interactive_profile}>
-            <img className={s.post_profile_picture} src={profilePicture} alt="NO PROFILE PICTURE"/>
+        <div onClick={handleProfileClick} className={style}>
+            <img className= {s.post_profile_picture} src={profilePicture} alt="NO PROFILE PIC"/>
             <strong className={s.username}>{userName}</strong>
         </div>
     );
@@ -39,9 +58,9 @@ function PostCaption({caption}){
 function PostMultimedia({multimedia, multimediaType}){
     return(
         <div className={s.multimedia}>
-            {multimediaType === "image" && <img src={multimedia} alt="No image source found" />}
-            {multimediaType === "video" && <video src={multimedia} controls alt="No video source found" />}
-            {multimediaType === "audio" && <audio src={multimedia} controls alt="No audio source found" />}
+            {multimediaType === "image" && <img src={multimedia} alt="No img source found" />}
+            {multimediaType === "video" && <video src={multimedia} controls alt="No vid source found" />}
+            {multimediaType === "audio" && <audio src={multimedia} controls alt="No aud source found" />}
         </div>
     )
 }
@@ -50,7 +69,7 @@ function PostInfoBar({likeCount, commentCount}){
     return (
         <div className={s.post_info_bar}>
             <div className={s.info_bar_left}>
-                <img src={likedButtonImg}/>
+                <img src={likedButtonImg} alt="Like Button"/>
                 <small className={s.like_count}>{likeCount} Likes</small>
             </div>
             <small className={s.comment_count}>{commentCount} Comments</small>
@@ -73,10 +92,11 @@ function LikeButton({ initialLiked }) {
     );
 }
 
-function PostInteractionBar(isLiked){
+function PostInteractionBar({isLiked}){
     return (
         <div className={s.interaction_bar}>
             <LikeButton />
+            <VerticalSeparator/>
             <button className={s.comment_button}>
                 Comment
             </button>
@@ -84,6 +104,111 @@ function PostInteractionBar(isLiked){
     );
 }
 
+function PostPreviewComments({post_id, commentsPopupHandler}){
+
+    const preview_comment_id = 3; // Use function to fetch from database using post_id
+
+    return(
+        <div className={s.post_preview_comments}>
+            <Comment comment_id={preview_comment_id}/>
+            <ShowAllCommentsButton post_id={post_id} commentsPopupHandler={commentsPopupHandler}/>
+        </div>
+    )
+}
+
+function Comment({comment_id}){
+
+    // const user_id = getUserByCommentId(comment_id) // Implement function on backend
+
+    const user_id = 3;
+    const content = "I am very proud of you my dear friend Koulis!"
+    return(
+        <div className={s.comment}>
+            <InteractiveProfile user_id={user_id} altern={true}/>
+            <div className={s.comment_content}>
+                {content}
+            </div>
+        </div>
+    )
+}
+
+function ShowAllCommentsButton({post_id, commentsPopupHandler}){
+
+    const handleClick = () => {
+        commentsPopupHandler.showCommentsPopup(post_id)
+    }
+
+    return (
+        <button className={s.show_comments_popup_button} onClick={handleClick}>
+            Show all comments
+        </button>
+    );
+}
+
+function CommentsPopup({commentsPopupHandler}){
+    // Fetch from database
+    // const comments = commentsData[post_id] || [];
+
+    // Fetch all comments and render them on a floating window
+
+    // MODIFY TO GET THE COMMENTS IN A LIST, AND RENDER THEM HERE (i think use .map, check)
+    if (commentsPopupHandler.selectedPostId === 3)
+        return(
+            <div className={commentsPopupHandler.isPopupVisible ? s.comments_popup_visible : s.comments_popup_invisible}>
+                <div className={s.comments_popup_header}>
+                    All Comments
+                    <button className={s.hide_comments_popup_button} onClick={commentsPopupHandler.hideCommentsPopup}>
+                        Hide comments
+                    </button>
+                </div>
+                <HorizontalSeparator />
+                <div className={s.comments_popup_container}>
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                </div>
+            </div>
+        );
+    else
+        return(
+            <div className={commentsPopupHandler.isPopupVisible ? s.comments_popup_visible : s.comments_popup_invisible}>
+                <div className={s.comments_popup_header}>
+                    All Comments
+                    <button className={s.hide_comments_popup_button} onClick={commentsPopupHandler.hideCommentsPopup}>
+                        Hide comments
+                    </button>
+                </div>
+                <HorizontalSeparator />
+                <div className={s.comments_popup_container}>
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                    <HorizontalSeparator />
+                    <Comment comment_id={3}/>
+                </div>
+            </div>
+    );
+}
 
 
 // Export all components
@@ -93,5 +218,7 @@ export {
     PostCaption,
     PostMultimedia,
     PostInfoBar,
-    PostInteractionBar
+    PostInteractionBar,
+    PostPreviewComments,
+    CommentsPopup
 }
--- linkedin-project/src/Pages/HomePage/HomePage.js
+++ linkedin-project/src/Pages/HomePage/HomePage.js
@@ -1,36 +1,48 @@
 import s from "./HomePageStyle.module.css";
 import NavBar from "../../Components/NavBar/NavBar";
+import PersonalDetailsPanel from "../../Components/PersonalDetailsPanel/PersonalDetailsPanel.js"
 import Post from "../../Components/PostComponent/Post";
-import postPFP from "../../Images/logo.png"
-import malakes from "../../Images/ergasia_welcome_page_image.png"
+import { useState } from "react";
+import { CommentsPopup } from "../../Components/PostComponent/PostComponents.js";
 
 function HomePage() {
+
+        // Comments popup state
+        const [isPopupVisible, setIsPopupVisible] = useState(false);
+        const [selectedPostId, setSelectedPostId] = useState(null);
+    
+        const showCommentsPopup = (post_id) => {
+            setIsPopupVisible(true);
+            setSelectedPostId(post_id)
+        };
+    
+        const hideCommentsPopup = () => {
+            setIsPopupVisible(false);
+            setSelectedPostId(null);            
+        };
+        
+        // Struct for efficiency
+        const commentsPopupHandler = {
+            showCommentsPopup,
+            hideCommentsPopup,
+            isPopupVisible,
+            selectedPostId
+        }
+
     return(
         <div className={s.home_page}>
             <NavBar isHome={true}/>
-            {/* Insert timeline component */}
-            <Post
-             profilePicture={postPFP}
-             userName={"Kyriakos Mitsotakis"}
-             timestamp={"16 Jul 2024"}
-             caption={"I am very excited to announce that i am joining Linkedin!"}
-             multimedia={malakes}
-             multimediaType={"image"}
-             likeCount={666}
-             commentCount={420}
-             isLiked={true}
-            />
-            <Post
-             profilePicture={postPFP}
-             userName={"Kyriakos Mitsotakis"}
-             timestamp={"16 Jul 2024"}
-             caption={"I am very excited to announce that i am joining Linkedin!"}
-             multimedia={malakes}
-             multimediaType={"image"}
-             likeCount={666}
-             commentCount={420}
-             isLiked={true}
-            />
+            <container className={s.container}>
+                <PersonalDetailsPanel />
+                <CommentsPopup commentsPopupHandler={commentsPopupHandler}/>
+                <timeline className={s.timeline}>
+                    <Post post_id={3} commentsPopupHandler={commentsPopupHandler}/>
+                    <Post post_id={2} commentsPopupHandler={commentsPopupHandler}/>
+                    <Post post_id={3} commentsPopupHandler={commentsPopupHandler}/>
+                </timeline>
+                
+            </container>
+            
         </div>
     );
 }
