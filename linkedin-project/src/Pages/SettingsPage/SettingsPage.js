943766597feb4ec904c539721997a4e704a4e107
Alexandros Theofylaktou <sdi2100220@di.uoa.gr>
2024-07-25 21:00:18 +0300

    Settings Page and scrollbar design
    Settings Page could use a little error message text under the input fields to inform the user what went wrong.

--- linkedin-project/src/Pages/SettingsPage/SettingsPage.js
+++ linkedin-project/src/Pages/SettingsPage/SettingsPage.js
@@ -8,34 +8,82 @@ import NavBar from './../../Components/NavBar/NavBar.js';
 import tsipras from "../../Images/tsipras.jpg"
 import mitsotakis from "../../Images/mitsotakis.jpg"
 
+const ConfirmPassword = ({correctPassword, IsConfirmingPassword, setIsConfirmingPassword}) => {
+
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [wrongInput, setWrongInput] = useState(false);
+    const [wrongInputCounter, setWrongInputCounter] = useState(0);
+    const [placeholderText, setPlaceholderText] = useState("Confirm your password");
+
+    const handleChange = (e) => {
+        setPlaceholderText("Confirm your password");
+        setCurrentPassword(e.target.value);
+        setWrongInput(false);
+    };
+
+    const handleConfirmPassword = () => {
+        // Correct password
+        if (correctPassword === currentPassword)
+            setIsConfirmingPassword(false);
+
+        // Wrong password
+        else if (currentPassword !== ""){
+            setWrongInput(true);
+            setCurrentPassword("")
+            const counter = wrongInputCounter + 1
+            setWrongInputCounter(counter);
+            (counter >= 3) ? setPlaceholderText("Too many attempts. Please try again later") : setPlaceholderText("Incorrect password");
+        }
+    };
+
+    const handleKeyDown = (e) =>{
+        if (e.key === "Enter"){
+            e.preventDefault();
+            handleConfirmPassword();
+        }
+    };
+
+    return (
+        <div className={s.setting_container}>
+            <label>Please confirm your password before you continue:</label>
+            <div className={s.password_field}>
+                <input 
+                    className={`${s.input_field} ${s.password_field} ${wrongInput ? s.wrong_input : ""}`}
+                    type={isPasswordVisible ? "text" : "password"}
+                    value={currentPassword}
+                    onChange={handleChange}
+                    onClick={handleChange}
+                    onKeyDown={handleKeyDown}
+                    placeholder={placeholderText}
+                    disabled={(wrongInputCounter >= 3) ? true : false}
+                />
+                <FontAwesomeIcon
+                    className={s.password_visibility_icon}
+                    onClick={() => {setIsPasswordVisible(!isPasswordVisible)}}
+                    icon={isPasswordVisible ? faEyeSlash : faEye}
+                    title={isPasswordVisible ? "Hide Password" : "Show Password"}
+                />
+            </div>
+            <button className={s.confirm_password_button} onClick={handleConfirmPassword}>
+                Confirm Password
+            </button>
+        </div>
+    )
+}
+
 const SettingControlBar = ({settingName, settingHandler}) => {
     
     console.log(settingHandler);
 
-    const {initial, previous, setPrevious, current,setCurrent,isChanging,setIsChanging} = settingHandler;
+    const {initial, previous, setPrevious, current,setCurrent,isChanging,setIsChanging, isWrong, setIsWrong, onConfirm, onCancel} = settingHandler;
     
     const handleSettingChange = () =>{
-        setIsChanging(true);
-    }
-
-    // Set state and set value back to initial
-    const cancelSettingChange = () =>{
-        setIsChanging(false);
-        setCurrent(previous);
-    }
-
-    const confirmSettingChange = () =>{
-        // if (settingName === "Email"){
-        //     if (!isEmail(current))
-        //         check email.
-        //         check for existing, not-email
-        // }
-        // else if (settingName === "Password"){
-        //      check if password is acceptable (use strong password regex, or if new password is same as initial)
-        //}
-        setIsChanging(false);
+        setCurrent("");
         setPrevious(current);
-    }
+        setIsChanging(true);
+        setIsWrong(false);
+    };
 
     return (
         <div className={s.control_bar}>
@@ -45,10 +93,10 @@ const SettingControlBar = ({settingName, settingHandler}) => {
                 </button>
             ) : 
             (<>
-                <button onClick={cancelSettingChange}>
+                <button onClick={onCancel}>
                     Cancel
                 </button>
-                <button onClick={confirmSettingChange}>
+                <button onClick={onConfirm}>
                     Confirm
                 </button>
             </>)}
@@ -72,26 +120,48 @@ function SettingsPage({user_id}) {
         if (user_id === 2)
             return "Kyriakos Mitsotakis"
     }
-
+    
     const getEmailAndPassword = (user_id) => {
         if (user_id === 3)
             return {
-                initialEmail: "tsipras.o.ntriplas@yahoo.com",
-                initialPassword: "MitsotakiGamiesai!"
-            };
-
-        if (user_id === 2)
-            return {
-                initialEmail: "mitsos.takis@gmail.com",
-                initialPassword: "JHx6z#hu&3"
-            };
+        initialEmail: "tsipras.o.ntriplas@yahoo.com",
+        initialPassword: "MitsotakiGamiesai!"
     };
+    
+    if (user_id === 2)
+        return {
+    initialEmail: "mitsos.takis@gmail.com",
+    initialPassword: "JHx6z#hu&3"
+};
+};
+
+    const [isConfirmingPassword, setIsConfirmingPassword] = useState(true);
 
     const {initialEmail,initialPassword} = getEmailAndPassword(user_id);
 
-    const [previousEmail, setPreviousEmail] = useState(initialEmail)
+    const [previousEmail, setPreviousEmail] = useState(initialEmail);
     const [currentEmail, setCurrentEmail] = useState(previousEmail);
     const [isEmailChanging, setIsEmailChanging] = useState(false);
+    const [isEmailWrong, setIsEmailWrong] = useState(false);
+
+    // If there is going to be a message display for errors, split these cases and use setPasswordErrorMessage()
+    const confirmEmailChange = () => {
+        // if is not email, setIsEmailWrong(true)
+        // if(isEmail(currentEmail)) (isEmail = function that returns if a string is considered an email (use regex))
+        if (currentEmail === "not an email" || currentEmail === ""){
+            setIsEmailWrong(true);
+        }
+        else{
+            setIsEmailChanging(false);
+            setPreviousEmail(currentEmail);
+        }
+    };
+
+    const cancelEmailChange = () => {
+        setIsEmailWrong(false);
+        setIsEmailChanging(false);
+        setCurrentEmail(previousEmail);
+    }
 
     // Struct for efficiency
     const emailHandler = {
@@ -101,13 +171,50 @@ function SettingsPage({user_id}) {
         current: currentEmail,
         setCurrent: setCurrentEmail,
         isChanging: isEmailChanging,
-        setIsChanging: setIsEmailChanging
+        setIsChanging: setIsEmailChanging,
+        isWrong: isEmailWrong,
+        setIsWrong: setIsEmailWrong,
+        onConfirm: confirmEmailChange,
+        onCancel: cancelEmailChange
     }
 
     const [previousPassword, setPreviousPassword] = useState(initialPassword)
     const [currentPassword, setCurrentPassword] = useState(previousPassword);
-    const [isPasswordChanging, setIsPaswordChanging] = useState(false);
+    const [currentConfirmPassword, setCurrentConfirmPassword] = useState("");
+    const [isPasswordChanging, setIsPasswordChanging] = useState(false);
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [isPasswordWrong, setIsPasswordWrong] = useState(false);
+
+    // Returns wether a value is considered a strong password or not
+    const isStrongPassword = (value) => {
+    // A strong password is a string that has a length of 8 - 16 characters and contains at least:
+    // > One capital letter
+    // > One lowercase letter
+    // > One number
+    // > One special character={!, @, $, %, ^, &, *, +, #}
+
+    // Use regex
+        return true;
+    }
+
+    // If there is going to be a message display for errors, split these cases and use setPasswordErrorMessage()
+    const confirmPasswordChange = () =>{
+        if (currentConfirmPassword !== currentPassword || currentPassword === "" || !isStrongPassword(currentPassword) || currentPassword === previousPassword){
+            setIsPasswordWrong(true);
+        }
+        else{
+            setIsPasswordChanging(false);
+            setPreviousPassword(currentPassword);
+        }
+        
+    };
+
+    const cancelPasswordChange = () => {
+        
+        setIsPasswordWrong(false);
+        setIsPasswordChanging(false);
+        setCurrentPassword(previousPassword);
+    }
 
     // Struct for efficiency
     const passwordHandler = {
@@ -117,7 +224,11 @@ function SettingsPage({user_id}) {
         current: currentPassword,
         setCurrent: setCurrentPassword,
         isChanging: isPasswordChanging,
-        setIsChanging: setIsPaswordChanging
+        setIsChanging: setIsPasswordChanging,
+        isWrong: isPasswordWrong,
+        setIsWrong: setIsPasswordWrong,
+        onConfirm: confirmPasswordChange,
+        onCancel: cancelPasswordChange
     }
 
     const profilePic = getProfilePicById(user_id);
@@ -127,10 +238,25 @@ function SettingsPage({user_id}) {
         setCurrentPassword(e.target.value);
     };
 
+    const handleConPwdChange = (e) => {
+        setCurrentConfirmPassword(e.target.value);
+    };
+
     const handleEmailChange = (e) => {
         setCurrentEmail(e.target.value);
     }
 
+    
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter"){
+            if (e.target.id === "emailInput")
+                confirmEmailChange();
+
+            if (e.target.id === "passwordInput" || e.target.id === "confirmPasswordInput")
+                confirmPasswordChange();
+        }
+    }
     
 
     return (
@@ -145,41 +271,72 @@ function SettingsPage({user_id}) {
                     </div>
 
                     <div className={s.settings_container}>
-
-                        <div className={s.setting_container}>
-                            <label htmlFor="emailInput">Email address</label>
-                            <input
-                                type="text"
-                                id="emailInput"
-                                className={s.input_field} 
-                                placeholder={currentEmail} 
-                                value={currentEmail}
-                                onChange={handleEmailChange}
-                                disabled={!isEmailChanging}
-                            />
-                            <SettingControlBar settingName="Email" settingHandler={emailHandler}/>
-                        </div>
-
-                        <div className={s.setting_container}>
-                            <label>Change password:</label>
-                            <div className={s.password_field}>
-                                <input 
-                                    className={`${s.input_field} ${s.password_field}`}
-                                    type={isPasswordVisible ? "text" : "password"}
-                                    value={currentPassword}
-                                    onChange={handlePwdChange}
-                                    disabled={!isPasswordChanging}
+                        {isConfirmingPassword ? 
+                            (
+                            <ConfirmPassword correctPassword={initialPassword} isConfirmingPassword={isConfirmingPassword} setIsConfirmingPassword={setIsConfirmingPassword} />
+                        ) : (
+                            <>
+                            <div className={s.setting_container}>
+                                <label htmlFor="emailInput">Email address:</label>
+                                <input
+                                    type="text"
+                                    id="emailInput"
+                                    className={`${s.input_field} ${isEmailWrong ? s.wrong_input : ""}`} 
+                                    placeholder={"Enter email address"} 
+                                    value={currentEmail}
+                                    onClick={handleEmailChange}
+                                    onChange={handleEmailChange}
+                                    onKeyDown={handleKeyDown}
+                                    disabled={!isEmailChanging}
                                 />
-                                <FontAwesomeIcon
-                                    className={s.password_visibility_icon}
-                                    onClick={() => {setIsPasswordVisible(!isPasswordVisible)}}
-                                    icon={isPasswordVisible ? faEyeSlash : faEye}
-                                    title={isPasswordVisible ? "Hide Password" : "Show Password"}
-                                />
-                             </div>
-                             <SettingControlBar settingName="Password" settingHandler={passwordHandler}/>
-                        </div>
-                    </div>
+                                <SettingControlBar settingName="Email" settingHandler={emailHandler}/>
+                            </div>
+
+                            <div className={s.setting_container}>
+                                <label>Password:</label>
+                                <div className={s.password_field}>
+                                    <input 
+                                        className={`${s.input_field} ${s.password_field} ${isPasswordWrong ? s.wrong_input : ""}`}
+                                        type={isPasswordVisible ? "text" : "password"}
+                                        id ="passwordInput"
+                                        placeholder={"Enter new password"}
+                                        value={currentPassword}
+                                        onClick={handlePwdChange}
+                                        onChange={handlePwdChange}
+                                        onKeyDown={handleKeyDown}
+                                        disabled={!isPasswordChanging}
+                                    />
+                                    <FontAwesomeIcon
+                                        className={s.password_visibility_icon}
+                                        onClick={() => {setIsPasswordVisible(!isPasswordVisible)}}
+                                        icon={isPasswordVisible ? faEyeSlash : faEye}
+                                        title={isPasswordVisible ? "Hide Password" : "Show Password"}
+                                    />
+                                </div>
+                                {isPasswordChanging ? (
+                                    <>
+                                        <label>Confirm Password:</label>
+                                        <div className={s.password_field}>
+                                            <input 
+                                                className={`${s.input_field} ${s.password_field} ${isPasswordWrong ? s.wrong_input : ""}`}
+                                                type={isPasswordVisible ? "text" : "password"}
+                                                id ="confirmPasswordInput"
+                                                placeholder={"Enter new password"}
+                                                value={currentConfirmPassword}
+                                                onClick={handleConPwdChange}
+                                                onChange={handleConPwdChange}
+                                                onKeyDown={handleKeyDown}
+                                                disabled={!isPasswordChanging}
+                                            />
+                                        </div>
+                                    </>
+                                ) : <></>}
+                                <SettingControlBar settingName="Password" settingHandler={passwordHandler}/>
+                            </div>
+                        </>
+                        )}
+                </div>
+                        
 
                 </div>
             </div>
